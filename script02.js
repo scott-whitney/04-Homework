da9d1406bf741ad3cad7b68ac9a4bdc4228e8575
Minroe <scott.whitney334@gmail.com>
2020-03-18 18:18:18 -0700

    figuring out this timer

--- script02.js
+++ script02.js
@@ -1,22 +1,36 @@
 const quizButton = document.getElementById('quizBtn')
 quizButton.addEventListener('click', startQuiz)
 
+var HighScores = [];
+// var highscores = [
+//     {
+//     score: 100, 
+//     name: "tommy", 
+//     time: 42.14
+//     },
 
-var highscores = [
-    {score: 100, name: "tommy", time: 42.14},
-
-];
+// ];
 
 function startQuiz() {
+
+
+    countDownTimer();
+    
     userScore = thisQuiz();
     userName = prompt("what are your initials?")
-
-    var serializedScores = JSON.stringify(highscores);
-
+    // var serializedScores = JSON.stringify(highscores);
+    HighScores.push({userScore, userName})
+    console.log(HighScores);
 }
 
 function thisQuiz() {
     var score = 0
+    // count down progress b
+
+
+
+
+
 
     var questions = [ {q: "Are Crosley's the first SUV?", a:true}, 
     {q: "Did Crosley make the first American Sports Car?", a:true}, 
@@ -43,26 +57,58 @@ function thisQuiz() {
         }
     }
 
-    alert(score + " " + "out of 10");
-    alert(`you got the score of ${score} out of ${questions.length}`)
+    // alert(score + " " + "out of 10");
+    // alert(`you got the score of ${score} out of ${questions.length}`)
 
     return score;
 
 }
 
 
+function countDownTimer() {
+
+    var count = 45;
+    var interval = setInterval(function(){
+        document.getElementById('count').innerHTML=count;
+        count--;
+        if (count === 0){
+          clearInterval(interval);
+          document.getElementById('count').innerHTML='Done';
+          // or...
+          alert("You're out of time!");
+        }
+      }, 1000);
+
+}
+
 
 
 // to show the scores:
-function showScores(parent, scores) {
-    // a <dl> could also be appropriate
-    var scoresElt = document.createElement('ol'),
-        i, scoreText;
-    scores.sort(function(a, b) {return b.score - a.score;});
-    for (i=0; i < scores.length; ++i) {
-        scoresElt.appendChild(document.createElement('li'));
-        scoreText = scores[i].score + ': ' + scores[i].name;
-        scoresElt.lastChild.appendChild(document.createTextNode(scoreText));
-    }
-    parent.appendChild(scoresElt);
-}
+// function showScores(parent, scores) {
+//     // a <dl> could also be appropriate
+//     var scoresElt = document.createElement('ol'),
+//         i, scoreText;
+//     scores.sort(function(a, b) {return b.score - a.score;});
+//     for (i=0; i < scores.length; ++i) {
+//         scoresElt.appendChild(document.createElement('li'));
+//         scoreText = scores[i].score + ': ' + scores[i].name;
+//         scoresElt.lastChild.appendChild(document.createTextNode(scoreText));
+//     }
+//     parent.appendChild(scoresElt);
+// }
+
+// function HighScores() {
+//     if(typeof(Storage)!=="undefined"){
+//         var scores = false;
+//         if(localStorage["high-scores"]){
+//             high_scores.style.display = "block";
+//             high_scores.innerHTML = '';
+//             scores = JSON.parse(localStorage["high-scores"]);
+//             scores = scores.sort(function(a,b){return parseInt(b)-parseInt(a)});
+
+//             for()
+//         }
+//     } else {
+//         high_scores.style.display = "none";
+//     }
+// }
